c4b3ed5a4d730e1bf65a7277df73b7d311e87543
Braedon Fredin <b260fredin@gmail.com>
2024-03-14 17:39:13 -0500

    [2.01] Set up start of program for user input.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,47 @@
 ﻿using NLog;
 
 // See https://aka.ms/new-console-template for more information
-string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+Console.Clear();
 logger.Info("Program started");
 
-string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-logger.Info(scrubbedFile);
-MovieFile movieFile = new MovieFile(scrubbedFile);
+string userChoice;
+do {
+    do {
+        Console.Write("\n CHOOSE:\n---------\n1) Add Movie\n2) Display All Movies\n\nEnter to quit\n\n> ");
+        userChoice = getUserChoice();
+
+        logger.Info("User choice: \"{0}\"", userChoice);        
+    } while (!verifyUserChoice(userChoice) && userChoice != "");
+
+} while (!verifyUserChoice(userChoice));
 
 logger.Info("Program ended");
+
+string getUserChoice() {
+    try {
+        userChoice = Console.ReadLine()
+                                .ToCharArray()[0]
+                                .ToString()
+        ;
+    } catch (IndexOutOfRangeException) {
+        userChoice = "";
+    }
+
+    return userChoice;
+}
+
+bool verifyUserChoice(string input) {
+    bool returnValue = false;
+
+    if(input == "1" || input == "2" || input == "") {
+        returnValue = true;
+    } else {
+        logger.Error("\nInvalid input: \"{0}\"\n", input);
+    }
+
+    return returnValue;
+}
